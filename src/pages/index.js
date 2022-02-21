8135077ef5b98b8018267418b88aac6a79ae7dc1
graycar23 <graycar23@gmail.com>
2022-02-21 17:54:36 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,17 @@ import * as React from 'react'
 
 import Layout from '../components/layout'
 
+import { StaticImage } from 'gatsby-plugin-image'
+
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="An image of various wrestlers from the wrestling company AEW."
+        src="../images/aew.jpg"
+      />
     </Layout>
   )
 }
