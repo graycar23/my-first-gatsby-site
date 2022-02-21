a02d089ee8a581acdbe5805383caaeb25cd62efa
graycar23 <graycar23@gmail.com>
2022-02-21 16:10:43 -0500

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+// Step 1: Import React
+import * as React from 'react'
+
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm Grayson Salyers, a senior computer science major here at EKU. I'm the proud creator of this site, which I built with Gatsby. I like playing video games and watching wrestling in my spare time.</p>
+    </Layout>
+  )
+}
+// Step 3: Export your component
+export default AboutPage
