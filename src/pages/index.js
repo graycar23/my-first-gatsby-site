0b702b2af5dd55491414c5ed6325a7fee4b204f9
graycar23 <graycar23@gmail.com>
2022-02-27 16:05:53 -0500

    Finished Gatsby Tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
-        alt="An image of various wrestlers from the wrestling company AEW."
+        alt="An promotional image of various wrestlers from the wrestling company AEW."
         src="../images/aew.jpg"
       />
     </Layout>
